96e622a2b8be2f9268f8e52ea208b442c2359e71
jakadamko <jakadamko@gmail.com>
2017-11-18 23:19:24 -0500

    11/18/2017

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var port = process.env.PORT || 3000;
+var PORT = process.env.PORT || 3000;
 //var routes = require('./routing/htmlRoutes');
 
 // Sets up the Express app to handle data parsing
